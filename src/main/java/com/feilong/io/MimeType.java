ef5114043d9f9b2b80e53d3f7599bc12305bf95c
feilong <venusdrogon@163.com>
2016-05-15 15:18:02 +0800

    update javadoc

--- src/main/java/com/feilong/io/MimeType.java
+++ src/main/java/com/feilong/io/MimeType.java
@@ -31,8 +31,8 @@ public enum MimeType{
      * 
      * <p>
      * 它基于ECMAScript的一个子集。<br>
-     * JSON采用完全独立于语言的文本格式，但是也使用了类似于C语言家族的习惯（包括C、C++、C#、Java、JavaScript、Perl、Python等）。这些特性使JSON成为理想的数据交换语言。<br>
-     * 易于人阅读和编写，同时也易于机器解析和生成(一般用于提升网络传输速率)。
+     * JSON采用完全独立于语言的文本格式,但是也使用了类似于C语言家族的习惯（包括C、C++、C#、Java、JavaScript、Perl、Python等）。这些特性使JSON成为理想的数据交换语言。<br>
+     * 易于人阅读和编写,同时也易于机器解析和生成(一般用于提升网络传输速率)。
      * </p>
      * 
      * <h3>JSON 语法规则:</h3>
